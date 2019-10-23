6bc9b8f3d581ed471f9060eacc382c2758705a0e
Chance Penner <chance.penner2016@ku.edu>
2019-10-23 10:52:29 -0500

    Add php read in from html

--- exercise2/CreateUser.php
+++ exercise2/CreateUser.php
@@ -1,1 +1,40 @@
-CreateUser.php
+<!-- CreateUser.php -->
+
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+echo "not broken yet";
+
+$id = $_POST["username"];
+
+$mysqli = new mysqli("mysql.eecs.ku.edu", "chancepenner2016", "eeRahL7v", "chancepenner2016");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$query = "SELECT Users, user_id FROM Users";
+
+if ($result = $mysqli->query($query)) {
+
+    /* fetch associative array */
+    while ($row = $result->fetch_assoc()) {
+        printf ("%s (%s)\n", $row["user_id"]);
+        echo 'hi';
+    }
+
+    /* free result set */
+    $result->free();
+}
+else
+{
+  $mysqli-> "INSERT INTO Users('user_id') VALUES ('$id')";
+  echo $id;
+  echo "\n";
+}
+
+$mysqli->close();
+/* close connection */
+?>
