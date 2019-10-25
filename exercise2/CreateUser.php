4a3cac99977a49215417819712f2fecda01bb0f9
Chance Penner <chance.penner2016@ku.edu>
2019-10-25 10:01:48 -0500

    add check for already existing username

--- exercise2/CreateUser.php
+++ exercise2/CreateUser.php
@@ -5,8 +5,22 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 $id = $_POST["username"];
+$usernameTaken = false; //gets set to true if username is already in the table
 
 $mysqli = new mysqli("mysql.eecs.ku.edu", "chancepenner2016", "eeRahL7v", "chancepenner2016");
+
+
+$query = mysqli_query($mysqli, "SELECT * FROM `Users` WHERE user_id='".$id."'");
+
+    if (!$query)
+    {
+        die('Error: ' . mysqli_error($mysqli));
+    }
+
+if(mysqli_num_rows($query) > 0){
+
+    $usernameTaken = true;
+}
 
 /* check connection */
 if ($mysqli->connect_errno) {
@@ -16,12 +30,12 @@ if ($mysqli->connect_errno) {
 
 $query = "SELECT Users, user_id FROM Users";
 
+
 if ($result = $mysqli->query($query)) {
-  echo "IF STATEMENT\n";
+  // echo "IF STATEMENT\n";
     /* fetch associative array */
     while ($row = $result->fetch_assoc()) {
         printf ("%s (%s)\n", $row["user_id"]);
-        echo 'hi';
     }
 
     /* free result set */
@@ -29,15 +43,20 @@ if ($result = $mysqli->query($query)) {
 }
 else
 {
+
   $sql= "INSERT INTO Users(user_id) VALUES ('$id')";
-  if($id != "")
+  if($id == "")
   {
-    $mysqli->query($sql);
-    echo "user " . $id . " was successfully created!\n";
+    echo "You left the text field blank. Please try again!\n";
+  }
+  else if($usernameTaken)
+  {
+    echo "User " . $id . " already exists. Please try again.\n";
   }
   else
   {
-    echo "Invalid username. Please try again!\n";
+    $mysqli->query($sql);
+    echo "User " . $id . " was successfully created!\n";
   }
 }
 
