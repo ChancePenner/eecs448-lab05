ea9182274074b8ec474b5ec97b493cbcb4646e90
Chance Penner <chance.penner2016@ku.edu>
2019-10-27 14:12:43 -0500

    Implement exercise 7

--- exercise6/ViewUserPosts.php
+++ exercise6/ViewUserPosts.php
@@ -1,1 +1,43 @@
 <!-- ViewUserPosts.php -->
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+$id = $_POST["username"];
+
+$mysqli = new mysqli("mysql.eecs.ku.edu", "chancepenner2016", "eeRahL7v", "chancepenner2016");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+
+$sql = "SELECT content FROM Posts WHERE author_id='$id'";
+$result = $mysqli->query($sql);
+
+if ($result->num_rows > 0) {
+  echo "<table width=10% border =\'1\'>";
+  echo "<th>" . $id . "'s Posts</th>";
+    /* fetch associative array */
+    while ($row = $result->fetch_assoc()) {
+        echo '<tr>';
+        echo '<td>';
+        echo $row["content"];
+        echo '</td>';
+        echo '</tr>';
+    }
+    echo "</table>";
+    /* free result set */
+    $result->free();
+}
+else
+{
+  echo "No posts found!";
+}
+
+$mysqli->close();
+/* close connection */
+
+?>
--- exercise7/DeletePost.php
+++ exercise7/DeletePost.php
@@ -1,1 +1,40 @@
 <!-- DeletePost.php -->
+
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+$post = $_POST["checkboxvar"];
+
+$mysqli = new mysqli("mysql.eecs.ku.edu", "chancepenner2016", "eeRahL7v", "chancepenner2016");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$arrlength = count($post);
+
+// $sql = "SELECT content FROM Posts WHERE author_id='$id'";
+// $result = $mysqli->query($sql);
+
+
+if ($arrlength > 0) {
+  for($i=0;$i<$arrlength;$i++)
+  {
+    $sql = "DELETE FROM Posts WHERE posts_id=$post[$i]";
+    $mysqli->query($sql);
+    echo "Deleted post " . $post[$i] . "<br>";
+  }
+
+}
+else
+{
+  echo "No post selected!";
+}
+
+$mysqli->close();
+/* close connection */
+
+?>
